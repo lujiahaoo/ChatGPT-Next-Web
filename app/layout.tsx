6d6618fafc625a6b386e34c659132a5a3f52b72a
Johopig <33998628+lujiahaoo@users.noreply.github.com>
2023-04-03 22:43:32 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,10 +17,10 @@ try {
 }
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "Johopig",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "Johopig",
     statusBarStyle: "black-translucent",
   },
   themeColor: "#fafafa",
